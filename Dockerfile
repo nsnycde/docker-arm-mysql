b981671013aaa026b5a7f95ee89439600e379ac6
James Bacon <james@baconi.co.uk>
2016-05-04 23:11:53 +0100

    Extra pre MySQL start up configuration

--- Dockerfile
+++ Dockerfile
@@ -16,11 +16,11 @@ COPY install-mysql.bash /opt/docker-arm-mysql/
 ## Install MySQL
 RUN /opt/docker-arm-mysql/install-mysql.bash
 
-## TODO - Upload MySQL configuration for docker script
-#COPY configure-mysql-for-docker.bash /opt/docker-arm-mysql/
+## Upload MySQL configuration for docker script
+COPY configure-mysql-for-docker.bash /opt/docker-arm-mysql/
 
-## TODO - Run MySQL configuration for docker script
-#RUN /opt/docker-arm-mysql/configure-mysql-for-docker.bash
+## Run MySQL configuration for docker script
+RUN /opt/docker-arm-mysql/configure-mysql-for-docker.bash
 
 ## Upload script to initialise MySQL
 COPY initialise-mysql-insecure.bash /opt/docker-arm-mysql/
--- configure-mysql-for-docker.bash
+++ configure-mysql-for-docker.bash
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
-## TODO - Enable external connections
+## Enable external connections
+sed -i 's/^bind-address.*/bind-address = 0.0.0.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+
+echo '' >> /etc/mysql/mysql.conf.d/mysqld.cnf
+echo '[mysqld]' >> /etc/mysql/mysql.conf.d/mysqld.cnf
+
+## Disable use of the internal host cache for faster name-to-IP resolution.
+## In this case, the server performs a DNS lookup every time a client connects.
+echo 'skip-host-cache' >> /etc/mysql/mysql.conf.d/mysqld.cnf
+
+## Do not resolve host names when checking client connections.
+## Use only IP addresses. If you use this option, all Host column values in the
+## grant tables must be IP addresses or localhost.
+echo 'skip-name-resolve' >> /etc/mysql/mysql.conf.d/mysqld.cnf
+
+echo '' >> /etc/mysql/mysql.conf.d/mysqld.cnf
