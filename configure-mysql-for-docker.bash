0296eb1815e187165da7918d53326133b8f62935
James Bacon <james@baconi.co.uk>
2016-05-05 00:06:43 +0100

    Updated configuration manually performed

--- configure-mysql-for-docker.bash
+++ configure-mysql-for-docker.bash
@@ -3,16 +3,13 @@
 ## Enable external connections
 sed -i 's/^bind-address.*/bind-address = 0.0.0.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf
 
+## Disable use of the internal host cache for faster name-to-IP resolution.
 echo '' >> /etc/mysql/mysql.conf.d/mysqld.cnf
 echo '[mysqld]' >> /etc/mysql/mysql.conf.d/mysqld.cnf
-
-## Disable use of the internal host cache for faster name-to-IP resolution.
-## In this case, the server performs a DNS lookup every time a client connects.
 echo 'skip-host-cache' >> /etc/mysql/mysql.conf.d/mysqld.cnf
-
-## Do not resolve host names when checking client connections.
-## Use only IP addresses. If you use this option, all Host column values in the
-## grant tables must be IP addresses or localhost.
-echo 'skip-name-resolve' >> /etc/mysql/mysql.conf.d/mysqld.cnf
-
 echo '' >> /etc/mysql/mysql.conf.d/mysqld.cnf
+
+## "Fix" problem where socket directory doesn't exist
+mkdir /var/run/mysqld
+chown mysql:mysql /var/run/mysqld
+chmod 700 /var/run/mysqld
